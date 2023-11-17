41fd605f9c8b196d0d2da9d7f1b23f28ecf208e1
JorgeBel <jorgeluizbelodelima@gmail.com>
2023-11-17 04:35:47 -0300

    ex12

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,6 +1,5 @@
 /// <reference types="cypress" />
-const { faker } = require('@faker-js/faker');
-const localidade = require ('./localidade.json')
+import EnderecoPage from "../support/page-ob/exercicio.page"
 
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
@@ -12,68 +11,30 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         Preenchendo todas opções no checkout
         E validando minha compra ao final */
 
-    beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
-
-    });
-
-    it('Deve adicionar o produto ao carrinho e fazer checkout até o final da compra!', () => {
-        var quantidade = 2
-        let nomeFaker = faker.name.firstName()
-        let sobrenomeFaker = faker.name.lastName()
-        let emailFaker = faker.internet.email(nomeFaker)
-
-        cy.get('[class="product-block grid"]')
-            .contains("Abominable Hoodie").click()
-        cy.get('.button-variable-item-S').click()
-        cy.get('.button-variable-item-Red').click()
-        cy.get('.input-text').clear().type(quantidade)
-        cy.get('.single_add_to_cart_button').click()
-
-
-
-
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
-        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+   
+    describe('Funcionalidade de produtos', () => {
         
-
-
-        cy.get('#primary-menu > .menu-item-629 > a').click()
-        cy.get('.post-2664 > .product-block > .caption > .meta > .infor > .name > a').click()
+        beforeEach(() => {
+            cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    
+        });
+    
+    it('Deve adicionar o produto ao carrinho e fazer checkout!', () => {
+        
+        cy.addProdutos('Abominable Hoodie', 'XS', 'Blue', 4)
         cy.get('.button-variable-item-XS').click()
-        cy.get('.button-variable-item-Orange').click()
-        cy.get('.input-text').clear().type("4").click()
-        cy.get('.single_add_to_cart_button').click()
 
+        ///
 
+        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+        EnderecoPage.editarNewCadastro("Flávio", "Caio", "Ebac", "Colômbia",
+        "Estrada das rosas", "Apartamento",
+        "São Paulo", "São Paulo", "37640000",
+        "999999999", "alunoebac@gmail.com")
 
-        cy.get('.woocommerce-message > .button').click()
-        cy.get('.cart_item > .product-name').should('contain', "Beaumont Summit Kit")
-        
-
-        cy.get('.checkout-button').click()
-
-        cy.addDados
-        cy.get('#billing_first_name').type(nomeFaker)
-        cy.get('#billing_last_name').type(sobrenomeFaker)
-        cy.get('#billing_company').type("Ebac")
-        cy.get('#select2-billing_country-container').click().type("Colômbia" + "{enter}")
-        cy.get('#billing_address_1').type("Estrada Casa Grande")
-        cy.get('#billing_address_2').type("Condomínio")
-        cy.get('#billing_city').type(localidade.cidade)
-        cy.get('#billing_state').type(localidade.estado)
-        cy.get('#billing_postcode').type("37640000")
-        cy.get('#billing_phone').type("999999999")
-        cy.get('#billing_email').type(emailFaker)
-        cy.get('#payment_method_cod').click()
-        cy.get('#terms').click()
-        cy.get('#place_order').click()
-        cy.get('.woocommerce-notice').should('contain', "Obrigado. Seu pedido foi recebido.")
-
-
+        });
     });
-
-
 });
 
 ///
